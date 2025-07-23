6e89198c293cbc5dd8181c6684cb33867dafd32d
Kevin Today <1142185+mieubrisse@users.noreply.github.com>
2025-07-23 17:44:04 -0300

    Shift beta to 0.08

--- src/lib/UserLogMARGuessingEngine.ts
+++ src/lib/UserLogMARGuessingEngine.ts
@@ -17,14 +17,12 @@ const MAXIMUM_LOGMAR_PRECISION: number = 1000;
 
   https://www.researchgate.net/publication/12066925_The_Slope_of_the_Psychometric_Function_for_Bailey-Lovie_Letter_Charts_Defocus_Effects_and_Implications_for_Modeling_Letter-By-Letter_Scores
   */
-// const LOGISTIC_PSYCHOMETRIC_BETA_LOGMAR_THOUSANDTHS: number = 0.015; // 15.0 converted for LogMAR thousandths
-const LOGISTIC_PSYCHOMETRIC_BETA_LOGMAR_THOUSANDTHS: number = 0.1; // 15.0 converted for LogMAR thousandths
+const LOGISTIC_PSYCHOMETRIC_BETA_LOGMAR_THOUSANDTHS: number = 0.08; // TODO determine this dynamically
 
 function normalizeVector(vector: number[]): number[] {
   const sum = vector.reduce((prevVal, curr) => prevVal + curr, 0);
   return sum ? vector.map((val) => val / sum) : vector;
 }
-
 
 // See https://en.wikipedia.org/wiki/Entropy_(information_theory)
 export function calculateShannonEntropy(probabilities: number[]): number {
