2d060aeb14936c9c8cf3933d4533b0a04fdb3f80
Ben Gazzard <ben@dartoxia.com>
2025-10-03 11:22:16 +0100

    Remove fastify wrapper

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,19 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import viteFastify from "@fastify/vite/plugin";
-import { resolve } from "node:path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  root: resolve(import.meta.dirname, "client"),
-  plugins: [
-    viteFastify({
-      clientModule: "/src/main.tsx",
-    }),
-    react(),
-  ],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
-});
+    plugins: [react()],
+})
