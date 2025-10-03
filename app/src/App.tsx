d97653adb12a299de5ed63da86ff57b602ec278a
Ben Gazzard <ben@dartoxia.com>
2025-10-03 11:02:45 +0100

    WIP firebase

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -8,6 +8,24 @@ import AssessmentPage from "./pages/AssessmentPage";
 import ResultsPage from "./pages/ResultsPage";
 import { ROUTES } from "./lib/routes";
 
+import { initializeApp,  } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Replace the following with your app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCO1XR2yqK249cv4he7u9kfKY2yn-23clM",
+    authDomain: "myopia-data-streams.firebaseapp.com",
+    projectId: "myopia-data-streams",
+    storageBucket: "myopia-data-streams.firebasestorage.app",
+    messagingSenderId: "546263451191",
+    appId: "1:546263451191:web:f3177e1254565e5bdc21b2",
+    measurementId: "G-HT81VC67L9"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+analytics.app.automaticDataCollectionEnabled = true;
+
 const App: React.FC = () => {
   return (
     <Router>
